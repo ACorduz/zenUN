3c8b9d9cb8b185df542a5577972d53fd2d041021
UnalKami <croncancio@unal.edu.co>
2024-04-10 09:49:12 -0500

    Correcciones Despligue 2
    Correcciones de sintaxis

--- render/build.sh
+++ render/build.sh
@@ -1,10 +1,13 @@
 # Exit on error
 ser -o errexit
 
+# Modify this line as needed for your package manager
 pip install -r requirements.txt
 
-pip manage.py collectstatic --no-input
+# Convert static asset files
+python manage.py collectstatic --no-input
 
-pip manage.py migrate
+# Apply any outstanding database migrations
+python manage.py migrate
 
-pip manage.py tailwind build
+python manage.py tailwind build
