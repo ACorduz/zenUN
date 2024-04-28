05ce99d62a05c55e1ab8bfbe0179d5a75886e504
cristian <cbarrerar@unal.edu.co>
2024-04-28 13:44:26 -0500

    Se agrego el path para la aplicación prestamos, y se modifico el back para la funcionalidad solicitar Prestamo

--- zenUN/urls.py
+++ zenUN/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path para urls de la aplicación "usuarios"
-    path('', include('usuarios.urls'))#Se importan las URLS de la aplicación usuarios
+    path('', include('usuarios.urls')),#Se importan las URLS de la aplicación usuarios
+    #path para urls de la aplicación "prestamos"
+    path('', include('prestamos.urls'))#Se importan las URLS de la aplicación prestamos
 ]
