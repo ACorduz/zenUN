6739b066bca4c2ef6b4dffb6e1a82e729dea5bd4
ACorduz <aorduz@unal.edu.co>
2024-04-29 18:11:20 -0500

    Creación de modelos SPRINT 2
    Se crean los modelos necesarios para el funcionamiento de las funcionalidades del sprint 2.

--- prestamos/models.py
+++ prestamos/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from usuarios.models import usuario
+from datetime import datetime
 
-# Create your models here.
+class edificio(models.Model):
+    idEdificio = models.AutoField(primary_key=True)
+    nombreEdificio = models.CharField(max_length=45)
+
+class implemento(models.Model):
+    idImplemento = models.AutoField(primary_key=True)
+    nombreImplemento = models.CharField(max_length=45)
+    estadoImplemento = models.CharField(max_length=45)
+    edificioId = models.ForeignKey(edificio, on_delete=models.CASCADE)
+
+class estadoPrestamo(models.Model):
+    idEstadoPrestamo = models.AutoField(primary_key=True)
+    nombreEstado = models.CharField(max_length=45)
+    descripcionEstado = models.CharField(max_length=255)
+
+class prestamo(models.Model):
+    idPrestamo = models.AutoField(primary_key=True)
+    estudianteNumeroDocumento = models.ForeignKey(usuario, on_delete=models.CASCADE, related_name='estudianteNumeroDocumento')
+    administradorBienestarNumeroDocumento = models.ForeignKey(usuario, null=True, on_delete=models.CASCADE, related_name='administradorBienestarNumeroDocumento')
+    fechaHoraCreacion = models.DateTimeField(default=datetime.now)
+    fechaHoraInicioPrestamo = models.DateTimeField()
+    fechaHoraFinPrestamo = models.DateTimeField()
+    idImplemento = models.ForeignKey(implemento, on_delete=models.CASCADE)
+    estadoPrestamo = models.ForeignKey(estadoPrestamo, on_delete=models.CASCADE)
+    comentario = models.TextField(null=True)
+
+class comentarioImplemento(models.Model):
+    idComentarioImplemento = models.AutoField(primary_key=True)
+    fechaHoraCreacion = models.DateTimeField(default=datetime.now)
+    comentario = models.TextField()
+    implementoId = models.ForeignKey(implemento, on_delete=models.CASCADE)
+
+
+
