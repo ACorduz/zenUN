ab7ff5a33140c500cc0d71f9f523f464550b21c5
Camila Amaya <86732477+CmilAmaya@users.noreply.github.com>
2024-05-24 18:09:08 -0500

    Configuración url y creacion vista de listaEventos

--- eventos/urls.py
+++ eventos/urls.py
@@ -3,9 +3,12 @@ from . import views
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-        path('asistirEvento/', 
-            views.mostrar_asistirEvento, 
-            name='asistirEvento'
-        ) #Vista asitirEvento
-           
+        path('eventos/', 
+            views.mostrar_listaEventos, 
+            name='eventos'
+        ), #Vista asitirEvento
+        path("asistirEvento/",
+             views.mostrar_asistirevento,
+             name='asistirEvento'
+             )
         ]
